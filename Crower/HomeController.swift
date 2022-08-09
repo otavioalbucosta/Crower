d32b1ce13c0976d6d5591b6d367542897ebff569
otavioalbucosta <otavio-albucosta@hotmail.com>
2022-08-09 10:17:03 -0300

    main screen created

---
+++ Crower/HomeController.swift
@@ -0,0 +1,8 @@
+//
+//  HomeController.swift
+//  Crower
+//
+//  Created by Otávio Albuquerque on 09/08/22.
+//
+
+import Foundation
---
+++ Crower/WordCell.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewCell.swift
+//  Crower
+//
+//  Created by Otávio Albuquerque on 09/08/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
