0cb0669809bb987ed9a7f2ef6d33271c68d4945d
otavioalbucosta <otavio-albucosta@hotmail.com>
2022-08-09 10:20:06 -0300

    main screen initated

--- Crower/HomeController.swift
+++ Crower/HomeController.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import UIKit
+
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    var cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+        collectionView?.backgroundColor = .black
+        
+        collectionView?.register(WordCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+}
+
--- Crower/WordCell.swift
+++ Crower/WordCell.swift
@@ -7,14 +7,37 @@
 
 import UIKit
 
-class CollectionViewCell: UICollectionViewCell {
+class WordCell: UICollectionViewCell {
+    
+    
     
     override init(frame: CGRect){
         super.init(frame: frame)
+        backgroundColor = .yellow
+        setupViews()
     }
+    
+    let wordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "ALO 123"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func setupViews() {
+        backgroundColor = .yellow
+        
+        addSubview(wordLabel)
+        wordLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        wordLabel.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        wordLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        wordLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        
     }
     
 }
