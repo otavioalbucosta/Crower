ad3f6c7958138fdb32b1ba2620d33576fd21a844
otavioalbucosta <otavio-albucosta@hotmail.com>
2022-08-10 10:31:17 -0300

    layout quase finalizado

---
+++ Crower/Service/API.swift
@@ -0,0 +1,10 @@
+//
+//  API.swift
+//  Crower
+//
+//  Created by Otávio Albuquerque on 10/08/22.
+//
+
+import Foundation
+
+
--- Crower/UserCellCollectionViewCell.swift
+++ Crower/UserCellCollectionViewCell.swift
@@ -11,7 +11,6 @@ class UserCellCollectionViewCell: UICollectionViewCell {
     
     override init(frame: CGRect){
         super.init(frame: frame)
-        backgroundColor = .yellow
         setupViews()
     }
     
@@ -29,17 +28,42 @@ class UserCellCollectionViewCell: UICollectionViewCell {
     let nameLabel: UILabel = {
         let label = UILabel()
         label.text = "TESTE TESTE"
+        label.backgroundColor = .green
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
         
         
     }()
     
+    let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "@username"
+        label.backgroundColor = .purple
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let bioTextView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .yellow
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    let followButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .cyan
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     func setupViews (){
-        backgroundColor = .yellow
         
         addSubview(profileImageView)
         addSubview(nameLabel)
+        addSubview(userNameLabel)
+        addSubview(bioTextView)
+        addSubview(followButton)
         
         profileImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 12).isActive = true
         profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 12).isActive = true
@@ -47,10 +71,24 @@ class UserCellCollectionViewCell: UICollectionViewCell {
         profileImageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
         
         nameLabel.topAnchor.constraint(equalTo: profileImageView.topAnchor, constant: 0).isActive = true
-        nameLabel.leftAnchor.constraint(equalTo: profileImageView.rightAnchor, constant: 5).isActive = true
+        nameLabel.leftAnchor.constraint(equalTo: profileImageView.rightAnchor, constant: 8).isActive = true
+        nameLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -12).isActive = true
+        nameLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        userNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+        userNameLabel.leftAnchor.constraint(equalTo: profileImageView.rightAnchor, constant: 8).isActive = true
+        userNameLabel.rightAnchor.constraint(equalTo: nameLabel.rightAnchor).isActive = true
+        userNameLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        bioTextView.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 10).isActive = true
+        bioTextView.leftAnchor.constraint(equalTo: nameLabel.leftAnchor).isActive = true
+        bioTextView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 40).isActive = true
+        bioTextView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        
+        followButton.topAnchor.constraint(equalTo: profileImageView.topAnchor).isActive = true
+        followButton.leftAnchor.constraint(equalTo: nameLabel.rightAnchor).isActive = true
         
         
-    
     }
     
 }
